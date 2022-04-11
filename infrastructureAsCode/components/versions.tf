6c0822bdd16b671eceeed1ec1b40a3c8e9041041
Michael Vanhee <michael.vanhee@icloud.com>
2022-04-11 18:59:02 +0200

    Update verions

--- infrastructureAsCode/components/versions.tf
+++ infrastructureAsCode/components/versions.tf
@@ -2,23 +2,23 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.1.0"
+      version = "2.32.0"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.5.0"
+      version = "1.3.2"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.10.0"
+      version = "1.13.2"
     }
     local = {
       source  = "hashicorp/local"
-      version = ">= 2.2.2"
+      version = "2.0.0"
     }
     null = {
       source  = "hashicorp/null"
-      version = ">= 3.1.1"
+      version = "3.0.0"
     }
   }
   required_version = ">= 1.1.8"
