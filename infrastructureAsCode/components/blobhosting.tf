ce79df7bbbc194df765ea98584ba885a23292ede
Michael Vanhee <michael.vanhee@icloud.com>
2022-04-14 10:23:28 +0200

    Rework blob

--- infrastructureAsCode/components/blobhosting.tf
+++ infrastructureAsCode/components/blobhosting.tf
@@ -6,4 +6,11 @@ resource "azurerm_storage_account" "static_website" {
   name                      = "${var.labname}materials"
   resource_group_name       = azurerm_resource_group.resourceGroup.name
   enable_https_traffic_only = true
+
+  allow_blob_public_access = true
+  static_website {
+    index_document = "index.html"
+    error_404_document = "error.html"
+  }
+
 }
